9f9ee6426bcb3ce75ffbe1e732026cb396d05b4b
ronakshivlab <ronak.patel@shivlab.com>
2024-03-07 18:00:26 +0530

    changes in file

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SimpleCustomAPI",
-            resources: [.copy("Resources")]
+            resources: [.process("Resources")]
         ),
 
     ]
