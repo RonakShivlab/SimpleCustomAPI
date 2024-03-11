65b9ea48237e4ca12a64007580517523d352c97d
ronakshivlab <ronak.patel@shivlab.com>
2024-03-11 16:17:59 +0530

    change in cell

--- Sources/SimpleCustomAPI/ViewControllers/LoginViewController.swift
+++ Sources/SimpleCustomAPI/ViewControllers/LoginViewController.swift
@@ -7,10 +7,16 @@
 
 import UIKit
 
-public class LoginViewController: UIViewController {
+public class LoginViewController: UIViewController, UITextFieldDelegate {
 
 //    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var lblLogin: UILabel!
+    @IBOutlet weak var tfEmailAddress: CustomTextField!
+    @IBOutlet weak var tfPassword: CustomTextField!
+    @IBOutlet weak var btnLogin: UIButton!
     public static let storyboardVC = UIStoryboard(name: "main", bundle: Bundle.module).instantiateInitialViewController()!
+    private let apiClient = GenericAPIClient<loginReponse>(baseURL: URL(string: "https://dummyjson.com/auth/")!)
     public override func viewDidLoad() {
         super.viewDidLoad()
 //        let nib = UINib(nibName: "TableViewCell", bundle: Bundle.module)
@@ -18,7 +24,36 @@ public class LoginViewController: UIViewController {
 //        self.tableView?.register(nib, forCellReuseIdentifier: "TableViewCell")
 //        self.tableView?.register(UINib(nibName: "TableViewCell", bundle: Bundle(for: LoginViewController.self)), forCellReuseIdentifier: "TableViewCell")
 //        self.tableView?.showsVerticalScrollIndicator = false
+        self.tfEmailAddress.delegate = self
+        self.tfPassword.delegate = self
         print("Done")
+    }
+    
+    @IBAction func btnLoginPressed(_ sender: UIButton) {
+        if !tfEmailAddress.validateEmail() {
+            tfEmailAddress.text = "Please enter a valid email address."
+        } else if !tfPassword.validatePassword() {
+            tfPassword.text = "Please enter a valid password."
+        } else if (tfEmailAddress.text == "kminchelle") && (tfPassword.text == "0lelplR"){
+            print("Success")
+            let loginRequest = loginRequest(username: tfEmailAddress.text ?? "", password: tfPassword.text ?? "")
+            self.callAPI(with: loginRequest)
+        }else{
+            print("username and password wrong")
+        }
+    }
+    func callAPI(with requestBody: loginRequest){
+        let postData = try? JSONEncoder().encode(requestBody)
+        print(postData as Any)
+        apiClient.callAPI(endpoint: "login", method: "POST", body: requestBody) { [self] result in
+            switch result{
+            case .success(let response):
+                print("Token: \(response.token)")
+                print("Response: \(response)")
+            case .failure(let error):
+                print("error \(error)")
+            }
+        }
     }
 }
 
